285989ba2911930381f070cd1fedd75388a538a0
Alexander <alexandermichaelmiranda@gmail.com>
2014-05-11 19:43:42 -0700

    Added more methods to textJustifier.js

--- src/textJustifier.js
+++ src/textJustifier.js
@@ -6,16 +6,27 @@ String.prototype.replaceAt = function(index, ch) {
 	return this.substr(0, index) + ch + this.substr(index + ch.length);
 };
 
+function substringModifier(substr, width){
+
+}
+
+function stringSplitter(string, width){
+	for(var i = width - 1; string[i] !== undefined; i = i + width + 1) {
+		if (string[i + 1] === " " && (i + 1) < string.length){
+			string = string.replaceAt((i + 1), '\n');
+		}
+		else {
+			substringModifier();
+		}
+	}
+	return string;
+}
+
 module.exports = function textJustifier(string, width) {
 	if (string.length <= width) {
 		return string;
 	}
-	for(var i = width - 1; string[i] !== undefined; i = i + width + 1) {
-		if (string[i] !== " " && (i + 1) < string.length){
-			string = string.replaceAt((i + 1), '\n');
-		}
-	}
-	return string;
+	return stringSplitter(string, width);
 };
 
 
