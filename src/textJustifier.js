f133bb31c92ca133f9e543c52badb35b122d32f7
Alexander <alexandermichaelmiranda@gmail.com>
2014-05-11 17:09:19 -0700

    Added string alteration method to textJustifier.js

--- src/textJustifier.js
+++ src/textJustifier.js
@@ -1,4 +1,19 @@
-module.exports = function textJustifier(string, width) {
-	
+/* Replace At method found on Stack Overflow at this link: 
+http://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-particular-index-in-javascript
+*/
+
+String.prototype.replaceAt = function(index, ch) {
+	return this.substr(0, index) + ch + this.substr(index + ch.length);
 };
+
+module.exports = function textJustifier(string, width) {
+	if (string.length <= width) {
+		return string;
+	}
+	if (string[width - 1] !== " ") {
+		string = string.replaceAt(width, '\n');
+		return string;
+	}
+};
+
 
--- test/textJustifier_test.js
+++ test/textJustifier_test.js
@@ -9,5 +9,11 @@ describe('textJustifier_test', function() {
 		it('function should return proper output', function() {
 			textJustifier("We hold these truths to be self-evident, that all men are created equal.", 20).should.equal("We hold these truths\nto  be self-evident,\nthat  all   men  are\ncreated equal.");
 		});
+		it('function should return string as is if length of width is longer than inputted string', function() {
+			textJustifier("Mom loves pie.", 20).should.equal("Mom loves pie.");
+		});
+		it('function should create a newline when string length exceeds the width length at a non-whitespace character', function(){
+			textJustifier("Bob loves food alot.", 14).should.equal("Bob loves food\nalot.");
+		});
 	});
 });
